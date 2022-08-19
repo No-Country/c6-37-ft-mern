92e293f3ed32586cb003b58400f86e61c223089f
Victor Falconi <v.falconi.m@gmail.com>
2022-08-18 21:55:21 -0500

    Correccion verificar usuario

--- back/controllers/usuarioController.js
+++ back/controllers/usuarioController.js
@@ -4,9 +4,9 @@ export const verifyUsuario = async (req, res) => {
     try {
         const { usuario, contrasena } = req.body;
         const usuarioEncontrado = await usuarioModel.findOne({usuario: usuario});
-        if(!usuarioEncontrado) return;
+        if(!usuarioEncontrado) {res.status(404).json({mesagge: 'Usuario no encontrado'}); return;}
         if(usuarioEncontrado.contrasena === contrasena) res.status(200).json({mesagge: 'Usuario autorizado'});
-        else res.status(404).json({mesagge: 'Usuario no encontrado'});
+        else res.status(404).json({mesagge: 'Contraseña incorrecta'});
     } catch (error) {
         res.status(404).json({message: error.message});
     }
